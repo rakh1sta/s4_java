21324c1c59b14e4f380ef3a7c451b5f18232930c
Rakhimova Shokhista <shokhista_rakhimova@student.itpu.uz>
2025-02-19 21:58:49 +0500

    flyway sql

--- src/main/resources/db/migration/V001__init.sql
+++ src/main/resources/db/migration/V001__init.sql
@@ -1,1 +1,49 @@
-create table user();
+CREATE TABLE student
+(
+    id     BIGSERIAL PRIMARY KEY,
+    name   VARCHAR(255) NOT NULL,
+    active BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE course
+(
+    id       BIGSERIAL PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
+    active   BOOLEAN DEFAULT FALSE,
+    start_at TIMESTAMP    NULL,
+    end_at   TIMESTAMP    NULL
+);
+
+CREATE TABLE department
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE discipline
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+ALTER TABLE student
+    ADD COLUMN department_id BIGINT,
+    ADD CONSTRAINT fk_student_department FOREIGN KEY (department_id) REFERENCES department (id);
+
+CREATE TABLE discipline_course
+(
+    course_id     BIGINT NOT NULL,
+    discipline_id BIGINT NOT NULL,
+    grading       INTEGER DEFAULT 0,
+    PRIMARY KEY (course_id, discipline_id),
+    CONSTRAINT fk_discipline_course_course FOREIGN KEY (course_id) REFERENCES course (id),
+    CONSTRAINT fk_discipline_course_discipline FOREIGN KEY (discipline_id) REFERENCES discipline (id)
+);
+CREATE TABLE grade
+(
+    student_id    BIGINT NOT NULL,
+    discipline_id BIGINT NOT NULL,
+    value         INT CHECK (value BETWEEN 0 AND 100),
+    PRIMARY KEY (student_id, discipline_id),
+    CONSTRAINT fk_grade_student FOREIGN KEY (student_id) REFERENCES student (id),
+    CONSTRAINT fk_grade_discipline FOREIGN KEY (discipline_id) REFERENCES discipline (id)
+);
